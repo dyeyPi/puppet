8bf098e6bd3431e6b4184950cafa62ba54159419
dyeyPi <jpandrey@gmail.com>
2020-07-10 21:38:53 +0800

    Add package resources and create user 'monitor'

--- hello.pp
+++ hello.pp
@@ -1,5 +1,31 @@
-file { '/root/Desktop/hello.txt':
-  ensure  => file,
-  content => "hello, Puppet\n",
+package{ 'vim-minimal':
+  ensure => present,
+}
+
+package{ 'curl':
+  ensure => present,
+}
+
+package{ 'git':
+  ensure => present,
+}
+
+group { 'devs':
+   gid =>  3000,
+}
+
+user { 'monitor':
+    ensure  =>  'present',
+    uid =>  '3001',
+    shell =>  '/bin/bash',
+    home =>  "/home/monitor",
+}
+
+file { '/home/monitor':
+   ensure => directory,
+   owner => 'monitor',
+   group => 'devs',
+   mode => 0750,
+   require => [User['monitor'], Group[devs]]
 }
 
